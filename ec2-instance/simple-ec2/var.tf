dc0b6a83ca331fb16ccfb44d524c634df7bad08b
Chaitanya <chai@gmail.com>
2024-09-07 03:30:22 +0530

    modified vars.tf

--- ec2-instance/simple-ec2/var.tf
+++ ec2-instance/simple-ec2/var.tf
@@ -1,19 +1,24 @@
 variable "ami" {
   description = "please enter the value"
+  default = "ami-0a866433198752a1e"
 }
 
 variable "subnet" {
   description = "enter the subnet_id"
+  default = "subnet-02ae83de918f27fc3"
 }
 
 variable "security_group_id" {
   description = "enter value during execution"
+  default = "sg-03073bb5a8e2803b8"
 }
 
 variable "instance_type" {
   description = "instance-type"
+  default = "t2.micro"
 }
 
 variable "instance-name" {
   description = "name of the instance"
+  default = "node3"
 }

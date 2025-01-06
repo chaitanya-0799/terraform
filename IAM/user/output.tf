da1cf819073e433e5970a6065a26d6761812454f
chaitanya-0799 <146313463+chaitanya-0799@users.noreply.github.com>
2025-01-06 13:35:44 +0530

    Update IAM/USER/output.tf

--- IAM/user/output.tf
+++ IAM/user/output.tf
@@ -5,7 +5,7 @@ output "access-key" {
 output "secret-key" {
   value = aws_iam_access_key.user1.secret
   sensitive = true
-  
+  # to get the value use command 'terraform output secret-key'
 }
 
 
@@ -16,4 +16,5 @@ output "user-id" {
 output "password" {
   value = aws_iam_user_login_profile.login.password
   sensitive = true
+  # to get the value use command 'terraform output password'
 }

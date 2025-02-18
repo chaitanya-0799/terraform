22b8b9dee01d22d8a3a43649ef5d34b638a2d2d6
chaitanya-0799 <146313463+chaitanya-0799@users.noreply.github.com>
2025-02-18 12:36:01 +0530

    Update main.tf

--- VPC/main_VPC/main.tf
+++ VPC/main_VPC/main.tf
@@ -1,162 +1,106 @@
-resource "aws_vpc" "demo-app" {
-  cidr_block = var.cidr-range
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
-    Name = var.vpc-name
+    Name                               = "${var.project_name}-vpc"
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
   }
 }
 
-resource "aws_subnet" "az1" {
-  vpc_id = aws_vpc.demo-app.id
-  cidr_block = var.cidr-az1
-  availability_zone = var.az-2a
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidr_blocks)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidr_blocks[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
   tags = {
-    Name = var.az1-name
+    Name = "${var.project_name}-public-${count.index+1}"
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
+    "kubernetes.io/role/internal-elb"              = "1"
+
   }
+
 }
 
-resource "aws_subnet" "pvt-az2a" {
-  vpc_id = aws_vpc.demo-app.id
-  cidr_block = var.pvt-2a-cidr
-  availability_zone = var.az-2a
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = var.pvt-2a-name
+    Name = "${var.project_name}-igw"
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
+    "kubernetes.io/role/internal-elb"              = "1"
   }
 }
 
-resource "aws_subnet" "pvt-az2b" {
-  vpc_id = aws_vpc.demo-app.id
-  cidr_block = var.pvt-2b-cidr
-  availability_zone = var.az-2b
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   tags = {
-    Name = var.pvt-2b-name
+    Name = "${var.project_name}-rt"
+     "kubernetes.io/cluster/${var.project_name}" = "shared"
+     "kubernetes.io/role/internal-elb"              = "1"
   }
 }
 
-resource "aws_subnet" "pvt-az2c" {
-  vpc_id = aws_vpc.demo-app.id
-  cidr_block = var.pvt-2c-cidr
-  availability_zone = var.az-2c
-  tags = {
-    Name = var.pvt-2c-name
-  }
+resource "aws_route_table_association" "name" {
+  count          = length(var.public_subnet_cidr_blocks)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.rt.id
 }
 
-resource "aws_subnet" "az2" {
-  vpc_id = aws_vpc.demo-app.id
-  cidr_block = var.cidr-az2
-  availability_zone = var.az-2b
+resource "aws_subnet" "private" {
+  count                   = length(var.public_subnet_cidr_blocks)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet_cidr_blocks[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
   tags = {
-    Name = var.az2-name
+    Name = "${var.project_name}-private-${count.index+1}"
+     "kubernetes.io/cluster/${var.project_name}" = "shared"
+    "kubernetes.io/role/internal-elb"              = "1"
   }
+
 }
 
-resource "aws_subnet" "az3" {
-  vpc_id = aws_vpc.demo-app.id
-  cidr_block = var.cidr-az3
-  availability_zone = var.az-2c
-  map_public_ip_on_launch = true
+resource "aws_eip" "nat" {
+  count = length(var.availability_zones)
+  domain = "vpc" 
   tags = {
-    Name = var.az3-name
+    Name = "${var.project_name}-nat-${count.index+1}"
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
+    "kubernetes.io/role/internal-elb"              = "1"
   }
 }
 
-resource "aws_internet_gateway" "IGW-app" {
-  vpc_id = aws_vpc.demo-app.id
+resource "aws_nat_gateway" "nat" {
+  count         = length(var.availability_zones)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
   tags = {
-    Name = var.IGW-name
+    Name = "${var.project_name}-nat-${count.index+1}"
+     "kubernetes.io/cluster/${var.project_name}" = "shared"
   }
 }
 
-resource "aws_route_table" "rt-2TierApp" {
-  vpc_id = aws_vpc.demo-app.id
+resource "aws_route_table" "private" {
+  count = length(var.private_subnet_cidr_blocks)
+  vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = var.rt-cidr
-    gateway_id = aws_internet_gateway.IGW-app.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat[count.index].id
   }
   tags = {
-    Name = var.rt-name
+    Name = "${var.project_name}-private-rt-${count.index+1}"
+     "kubernetes.io/cluster/${var.project_name}" = "shared"
   }
 }
 
-resource "aws_route_table_association" "rt-az1" {
-  subnet_id = aws_subnet.az1.id
-  route_table_id = aws_route_table.rt-2TierApp.id
-}
-
-resource "aws_route_table_association" "rt-az2" {
-  subnet_id = aws_subnet.az2.id
-  route_table_id = aws_route_table.rt-2TierApp.id
-}
-
-resource "aws_route_table_association" "rt-az3" {
-  subnet_id = aws_subnet.az3.id
-  route_table_id = aws_route_table.rt-2TierApp.id
-}
-
-resource "aws_eip" "eip" {
-  vpc = true
-  tags = {
-    Name = "nat-eip"
-  }
-}
-
-resource "aws_nat_gateway" "demo-NAT" {
-  subnet_id = aws_subnet.az1.id
-  allocation_id = aws_eip.eip.id
-  tags = {
-    Name = var.nat-name
-  }
-}
-
-resource "aws_route_table" "pvt-rt" {
-  vpc_id = aws_vpc.demo-app.id
-  route {
-    cidr_block = var.rt-cidr
-    gateway_id = aws_nat_gateway.demo-NAT.id
-  }
-  tags = {
-    Name = var.pvt-rt
-  }
-}
-
-resource "aws_route_table_association" "pvt-rt-1" {
-  subnet_id = aws_subnet.pvt-az2a.id
-  route_table_id = aws_route_table.pvt-rt.id
-}
-
-resource "aws_route_table_association" "pvt-rt-2" {
-  subnet_id = aws_subnet.pvt-az2b.id
-  route_table_id = aws_route_table.pvt-rt.id
-}
-
-resource "aws_route_table_association" "pvt-rt-3" {
-  subnet_id = aws_subnet.pvt-az2c.id
-  route_table_id = aws_route_table.pvt-rt.id
-}
-
-resource "aws_security_group" "sg-2TierApp" {
-  vpc_id = aws_vpc.demo-app.id
-
-  ingress {
-    to_port     = 0
-    from_port   = 0
-    protocol    = "-1" 
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    to_port = 0
-    from_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = var.sg-name
-  }
+resource "aws_route_table_association" "pvt-rt" {
+  count = length(var.private_subnet_cidr_blocks)
+  subnet_id = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }

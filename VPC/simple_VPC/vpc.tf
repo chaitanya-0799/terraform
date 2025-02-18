e90c7c02d0fa92d80495139e7ba302dca3e0b4a2
chaitanya-0799 <146313463+chaitanya-0799@users.noreply.github.com>
2025-02-18 12:45:16 +0530

    Update vpc.tf

--- VPC/simple_VPC/vpc.tf
+++ VPC/simple_VPC/vpc.tf
@@ -1,93 +1,52 @@
-resource "aws_vpc" "tf-vpc" {
-  cidr_block = var.vpc_cidr
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
-    Name = "tf-vpc"
+    Name                               = "${var.project_name}-vpc"
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
   }
 }
 
-data "aws_availability_zones" "available" {}
-
-resource "aws_subnet" "subnet-1" {
-  vpc_id = aws_vpc.tf-vpc.id
-  cidr_block = var.subnet-1_cidr
-  availability_zone = data.aws_availability_zones.available.names[0]
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidr_blocks)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidr_blocks[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
   tags = {
-    Name = "subnet-${data.aws_availability_zones.available.names[0]}"
+    Name = "${var.project_name}-public-${count.index+1}"
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
+    "kubernetes.io/role/internal-elb"              = "1"
+
+  }
+
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.project_name}-igw"
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
+    "kubernetes.io/role/internal-elb"              = "1"
   }
 }
 
-resource "aws_subnet" "subnet-2" {
-  vpc_id = aws_vpc.tf-vpc.id
-  cidr_block = var.subnet-2_cidr
-  availability_zone = data.aws_availability_zones.available.names[1]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "subnet-${data.aws_availability_zones.available.names[1]}"
-  }
-}
-
-resource "aws_subnet" "subnet-3" {
-  vpc_id = aws_vpc.tf-vpc.id
-  cidr_block = var.subnet-3_cidr
-  availability_zone = data.aws_availability_zones.available.names[2]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "subnet-${data.aws_availability_zones.available.names[2]}"
-  }
-}
-
-resource "aws_internet_gateway" "tf-igw" {
-  vpc_id = aws_vpc.tf-vpc.id
-  tags = {
-    Name = "tf-igw"
-  }  
-}
-
-resource "aws_route_table" "tf-public-route" {
-  vpc_id = aws_vpc.tf-vpc.id
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.tf-igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
-}
-
-resource "aws_route_table_association" "rt-subnet-1" {
-  subnet_id = aws_subnet.subnet-1.id
-  route_table_id = aws_route_table.tf-public-route.id 
-}
-
-resource "aws_route_table_association" "rt-subnet-2" {
-  subnet_id = aws_subnet.subnet-2.id
-  route_table_id = aws_route_table.tf-public-route.id 
-}
-
-resource "aws_route_table_association" "rt-subnet-3" {
-  subnet_id = aws_subnet.subnet-3.id
-  route_table_id = aws_route_table.tf-public-route.id 
-}
-
-resource "aws_security_group" "tf-sg" {
-  vpc_id = aws_vpc.tf-vpc.id
-  ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = [ "0.0.0.0/0" ]
-    ipv6_cidr_blocks = [ "::/0" ]
-  }
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = [ "0.0.0.0/0" ]
-    ipv6_cidr_blocks = [ "::/0" ]
-    
-  }
-
   tags = {
-    Name = "tf-sg"
+    Name = "${var.project_name}-rt"
+     "kubernetes.io/cluster/${var.project_name}" = "shared"
+     "kubernetes.io/role/internal-elb"              = "1"
   }
+}
+
+resource "aws_route_table_association" "name" {
+  count          = length(var.public_subnet_cidr_blocks)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.rt.id
 }

5b3cf8b8c9b8e5e200e3ff2ada85727cf62325dc
chaitanya-0799 <146313463+chaitanya-0799@users.noreply.github.com>
2025-02-18 12:37:37 +0530

    Update variable.tf

--- VPC/main_VPC/variable.tf
+++ VPC/main_VPC/variable.tf
@@ -1,94 +1,25 @@
-variable "aws-region" {
-  description = "aws region"
-}
-variable "cidr-range" {
-  description = "cidr range of vpc"
+variable "vpc_cidr_block" {
+  type = string
+  default = "18.0.0.0/16"
 }
 
-variable "vpc-name" {
-  description = "Name for VPC"
+variable "public_subnet_cidr_blocks" {
+  type = list(string)
+  default = ["18.0.1.0/24", "18.0.2.0/24", "18.0.3.0/24"]
 }
 
-variable "cidr-az1" {
-  description = "cidr range for subnet in az1"
+variable "private_subnet_cidr_blocks" {
+  type = list(string)
+  default = ["18.0.4.0/24","18.0.5.0/24", "18.0.6.0/24"]
 }
 
-variable "az-2a" {
-  description = "Naming convention az-a in hyd"
+variable "availability_zones" {
+  type = list(string)
+  default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
 }
 
-variable "az-2b" {
-  description = "Naming convention az-b in hyd"
+variable "project_name" {
+  type = string
+  default = "open-telementary"  
 }
 
-variable "az-2c" {
-  description = "Naming convention az-c in hyd"
-}
-
-variable "az1-name" {
-  description = "Name for subnet in az1"
-}
-
-variable "cidr-az2" {
-  description = "cidr range for subnet in az2"
-}
-
-variable "az2-name" {
-  description = "Name for subnet in az2"
-}
-
-variable "cidr-az3" {
-  description = "cidr range for subnet in az3"
-}
-
-variable "az3-name" {
-  description = "Name for subnet in az3"
-}
-
-variable "pvt-2a-cidr" {
-  description = "cidr range for pvt subnet in 2a"
-}
-
-variable "pvt-2a-name" {
-  description = "name for pvt subnet in 2a"
-}
-
-variable "pvt-2b-cidr" {
-  description = "cidr range for pvt subnet in 2b"
-}
-
-variable "pvt-2b-name" {
-  description = "name for pvt subnet in 2b"
-}
-
-variable "pvt-2c-cidr" {
-  description = "cidr range for pvt subnet in 2c"
-}
-
-variable "pvt-2c-name" {
-  description = "name for pvt subnet in 2c"
-}
-
-variable "IGW-name" {
-  description = "name for IGW"
-}
-
-variable "nat-name" {
-  description = "NAT name"
-}
-
-variable "rt-name" {
-  description = "Name for RT"
-}
-
-variable "rt-cidr" {
-  description = "cidr for igw"
-}
-
-variable "pvt-rt" {
-  description = "pvt-rt name"
-}
-
-variable "sg-name" {
-  description = "name of security group"
-}
